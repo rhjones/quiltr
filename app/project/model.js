1f2db485b82f512a6032c3cbba298ab2e198ce3e
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-05 16:52:27 -0400

    Generate project and projects routes
    Generate project and projects routes
    Generate project model and connect to pattern model

---
+++ app/project/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  notes: DS.attr('string'),
+  startDate: DS.attr('date'),
+  finishDate: DS.attr('date'),
+  finished: DS.attr('boolean'),
+  pattern: DS.belongsTo('pattern')
+});
