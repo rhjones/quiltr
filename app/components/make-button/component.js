0c95a611f752e39c35fd6025fce5b656fcc7a250
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-06 15:08:38 -0500

    Add notes on nested components/templates to make button
    Add comments to make-button/component.js about where component is used

--- app/components/make-button/component.js
+++ app/components/make-button/component.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
+// make-button <-- pattern-card <-- gallery (patterns)
+// make-button <-- pattern
+
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
