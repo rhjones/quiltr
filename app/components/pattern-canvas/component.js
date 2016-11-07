a61d928f2af0150af02246aed7cffe3f931e80ed
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-07 10:08:03 -0500

    Remove commented out code from pattern-canvas
    Clean up file

--- app/components/pattern-canvas/component.js
+++ app/components/pattern-canvas/component.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   classNames: ['canvas-container'],
-
-  didInsertElement() {
-    this._super(...arguments);
-    // let container = this.$();
-    // let vanilla = document.getElementsByClassName('canvas-container')[0];
-    // console.log('container', container);
-    // console.log('container.height', container.height());
-    // console.log('vanilla', vanilla);
-    // console.log('vanilla.height', vanilla.height);
-    // let canvas = this.$('#canvas');
-    // console.log('canvas', canvas);
-    // resizeCanvas.resize(container, canvas);
-  },
 });
