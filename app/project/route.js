a498c2aaa2bff2e3e304ecae9ff558c52fed5501
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-07 15:22:48 -0500

    Get pattern with project
    Load pattern data along with project so it can be displayed on project page

--- app/project/route.js
+++ app/project/route.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 // project --> pattern-card
 export default Ember.Route.extend({
-    model (params) {
+  model (params) {
     return this.get('store').findRecord('project', params.project_id);
   },
+  afterModel(model, transition) {
+    return model.get('pattern');
+  }
 });
