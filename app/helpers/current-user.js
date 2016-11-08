fe06451971da5d9ac1ded0bf95dc3875fbf4ad0b
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-07 21:55:15 -0500

    Remove commented out code

--- app/helpers/current-user.js
+++ app/helpers/current-user.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
 
-// export function currentUser(params/*, hash*/) {
-//   // expects a user id
-//   // auth: Ember.inject.service();
-//   // let 
-//   let ownerId = params[0];
-//   // return ownerId === Ember.inject.service('auth').credentials.id;
-//   return Ember.inject.service('auth').credentials.username;
-
-// }
-
 export default Ember.Helper.extend({
   auth: Ember.inject.service(),
-
   compute(params, hash) {
     // expects a user id as the only param
     let auth = this.get('auth');
