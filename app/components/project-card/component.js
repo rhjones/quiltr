af19273bc5f393eeb5c7774becb8e75eee0b813b
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-08 15:57:25 -0500

    Style project cards within pattern view
    Adjust bootstrap column classes for better responsiveness in different contexts (don't fix to component; rather, apply when component is used)

--- app/components/project-card/component.js
+++ app/components/project-card/component.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 
 // project-card <-- pattern
-// project-card <-- projects
+// project-card <-- projects (styled)
 // project-card <-- my-projects
 export default Ember.Component.extend({
-  classNames: ['col-sm-4', 'col-md-3'],
 });
