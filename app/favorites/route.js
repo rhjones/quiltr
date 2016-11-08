c0a16d2256424873b77b9ea33f0e7b28a43b0286
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-08 11:38:20 -0500

    Use improved toggleFavorite() in favorites
    Refactor favorites route & template to use toggleFavorite() action in favorite-button component

--- app/favorites/route.js
+++ app/favorites/route.js
@@ -13,22 +13,6 @@ export default Ember.Route.extend({
   //   Ember.RSVP.all(model.getEach('pattern'));
   // },
   actions: {
-    toggleFavorite(pattern) {
-      if (pattern.get('isFavorite')) {
-        let currentFavorite = pattern.get('currentFavorite');
-        currentFavorite.deleteRecord();
-        currentFavorite.save()
-        .then(() => {
-          this.get('flashMessages').success('Favorite removed.');
-        });
-      }
-      else {
-        let favorite = this.get('store').createRecord('favorite', {
-          pattern: pattern
-        });
-        favorite.save();
-      }
-    },
     createNewProject(pattern) {
       this.transitionTo('newproject', pattern);
     },
