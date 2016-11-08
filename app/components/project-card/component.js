deb65ea6749ee6315892e9eac70449afe8cb2054
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-08 15:38:23 -0500

    Style project card view
    Style project card view to display as tile with single image (or missing image icon)

--- app/components/project-card/component.js
+++ app/components/project-card/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
+// project-card <-- pattern
+// project-card <-- projects
+// project-card <-- my-projects
 export default Ember.Component.extend({
+  classNames: ['col-sm-4', 'col-md-3'],
 });
