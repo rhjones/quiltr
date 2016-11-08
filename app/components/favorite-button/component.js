7aa0878fa538d439266c0a4462d008e5eaf776da
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-08 11:19:53 -0500

    Refactor favorite button component
    Inject store into favorite button component
    Instead of sending action, toggle favorite (add/delete favorite record) directly from component
    
    Why this approach? After talking with @jrhorn, we decided this makes the most sense because:
    
    - component is used in multiple other components & routes
    - CRUD action involves adding/deleting a row from a join table
    - See https://github.com/ga-wdi-boston/capstone-project/issues/305#issuecomment-259152171 for full details

--- app/components/favorite-button/component.js
+++ app/components/favorite-button/component.js
@@ -2,17 +2,35 @@ import Ember from 'ember';
 
 // favorite-button <-- pattern-card <-- gallery (patterns)
 // favorite-button <-- pattern
+// favorite-button <-- pattern-card <-- favorites
+
+
+// 
 export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+  store: Ember.inject.service(),
   tagName: 'span',
   classNameBindings: ['isFavorite'],
-  isFavorite: Ember.computed.alias('pattern.isFavorite'),
-  currentFavorite: Ember.computed.alias('pattern.currentFavorite'),
-  auth: Ember.inject.service(),
+  isFavoritedByCurrentUser: Ember.computed.alias('pattern.isFavoritedByCurrentUser'),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
   actions: {
     toggleFavorite() {
-      // TODO if not authenticated, force sign in & then continue
-      this.sendAction('toggleFavorite', this.get('pattern'));
+      let favorite = this.get('favorite');
+      if (favorite) {
+        favorite.destroyRecord()
+        .catch(() => {
+          this.get('flashMessages').danger('There was an error. Please try again.');
+        })
+      }
+      else {
+        let favorite = this.get('store').createRecord('favorite', {
+          pattern: this.get('pattern'),
+        });
+        favorite.save()
+        .catch(() => {
+          this.get('flashMessages').danger('There was an error. Please try again.');
+        })
+      }
     }
   }
 
